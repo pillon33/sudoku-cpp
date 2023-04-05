5c203d16a8626b946ee6008a619eb28f52ac6cbe
= <=>
2023-04-05 20:24:12 +0200

    initialize project, general game layout

---
+++ sudoku/mainwindow.cpp
@@ -0,0 +1,41 @@
+
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::on_startGameButton_clicked()
+{
+}
+
+
+void MainWindow::on_button1_clicked()
+{
+
+}
+
+
+void MainWindow::on_button2_clicked()
+{
+
+}
+
+
+void MainWindow::on_button3_clicked()
+{
+
+}
+
